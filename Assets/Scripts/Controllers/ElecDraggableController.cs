6c439dbc1a0af719180bd71f9b18d98ab6638aca
BloonZhang <jeffrey.l.zhang@vanderbilt.edu>
2021-06-12 16:35:40 -0500

    Grid is draggable

---
+++ Assets/Scripts/Controllers/ElecDraggableController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ElecDraggableController : MonoBehaviour
+{
+    [SerializeField] int mouseButton = 2; // 0 = left, 1 = right, 2 = middle
+
+    // GameObjects
+    [SerializeField] GameObject grid;
+
+    // helper
+    bool isDragActive = false;
+    bool isMouseOver = false;
+    Vector3 offset = new Vector3();
+
+    void Update()
+    {
+        // On click
+        if (Input.GetMouseButtonDown(mouseButton) && isMouseOver)
+        {
+            isDragActive = true;
+            offset = grid.transform.position - 
+                        Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 
+                                                                   Input.mousePosition.y, 
+                                                                   0));
+                                                                   
+        }
+
+        // On release
+        if (Input.GetMouseButtonUp(mouseButton))
+        {
+            isDragActive = false;
+        }
+
+        if (isDragActive)
+        {
+            Debug.Log("dragging");
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            grid.transform.position = curPosition;
+        }
+    }
+
+    public void OnMouseEnter()
+    {
+        isMouseOver = true;
+    }
+    public void OnMouseExit()
+    {
+        isMouseOver = false;
+    }
+
+}
---
+++ Assets/Scripts/Controllers/WireMasterController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WireMasterController : MonoBehaviour
+{
+    //////// Singleton shenanigans ////////
+    private static WireMasterController _instance;
+    public static WireMasterController Instance { get {return _instance;} }
+    //////// Singleton shenanigans continue in Awake() ////
+
+    [SerializeField] GameObject gridObject;
+
+
+
+
+    void Awake()
+    {
+        // Singleton shenanigans
+        if (_instance != null && _instance != this) {Destroy(this.gameObject);} // no duplicates
+        else {_instance = this;}
+    }
+
+}
